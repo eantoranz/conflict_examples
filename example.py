3eb60f8e270eff783fac38d4a84e898966a51af4
Developer A <dev.a@localhost>
2020-03-19 21:42:45 -0600

    A little refactor (moving condition to another function)

--- example.py
+++ example.py
@@ -7,13 +7,13 @@ colors = {"black": "black mirror",
           "blue": "blue sky"}
 
 def getPhrase(color):
-    if color not in colors:
-        sys.stderr.write("Got no phrase for color %s\n" % color)
-        sys.exit(1)
     phrase = getFormattedPhrase(color)
     return phrase
 
 def getFormattedPhrase(aColor):
+    if aColor not in colors:
+        sys.stderr.write("Got no phrase for color %s\n" % aColor)
+        sys.exit(1)
     return "%s: %s" % (aColor, colors[aColor])
 
 print(getPhrase(sys.argv[1]))
