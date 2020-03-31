097e5417a0414d1d12ac87dc6ac39205f0f2a60d
Developer A <dev.a@localhost>
2020-03-30 19:01:35 -0600

    Common ancestor

---
+++ example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+
+colors = {"black": "black mirror",
+          "white": "white noise",
+          "blue": "blue sky"}
+
+def getPhrase(color):
+    color = color.lower()
+    if color not in colors:
+        sys.stderr.write("There is no phrase defined for color %s\n" % color)
+        sys.exit(1)
+    phrase = colors[color]
+    phrase = "%s: %s" % (color, phrase)
+    return phrase
+
+for color in sys.argv[1:]:
+    print(getPhrase(color))
