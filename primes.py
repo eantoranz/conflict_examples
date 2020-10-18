ea81ba7ebbdaf5f3647390c1be637fb4bb9f2ae4
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2020-10-17 23:34:06 -0600

    Check only already-known primes as divisors

--- primes.py
+++ primes.py
@@ -6,14 +6,17 @@
 
 # a script to list the primes up to 100
 
+primes=[]
 
 def isPrime(aNumber):
   prime=True
-  for i in range(2, aNumber):
+  for i in primes:
     prime=aNumber % i > 0
     if not prime:
       break
   # if we made it this far, it's a prime
+  if prime:
+    primes.append(aNumber)
   return prime
 
 for i in range(2, 100):
