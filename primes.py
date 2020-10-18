55be07315dc9cf289edcd1573cfb1a0988ffe75b
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2020-10-17 23:28:45 -0600

    Prime number calculation

---
+++ primes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# copyright 2020 Edmundo Carmona Antoranz
+# released under the terms of GPLv2
+
+
+# a script to list the primes up to 100
+
+
+def isPrime(aNumber):
+  prime=True
+  for i in range(2, aNumber):
+    prime=aNumber % i > 0
+    if not prime:
+      break
+  # if we made it this far, it's a prime
+  return prime
+
+for i in range(2, 100):
+  if isPrime(i):
+    print(i)
